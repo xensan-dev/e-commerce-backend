f7e8009c6f7a42b7aa326f142703c963e449feb4
BuildTools <unconfigured@null.spigotmc.org>
2023-02-07 01:02:29 -0800

    completed model datatypes. currently working on api routes and functionality.

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNul: false,
+      primaryKey: true, 
+      autoIncrement: true, 
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false, 
+      validate: {
+        isDecimal: true
+      }
+    }, 
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNul: false,
+      defaultValue: 10,
+      validate:{
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
